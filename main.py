546d8bf3973f15a8996f0d4fd684902f7a29b841
BartoszLiss <barteklisssss@gmail.com>
2026-04-16 16:01:29 +0200

    Main update

---
+++ main.py
@@ -0,0 +1,20 @@
+def main():
+    Nanostructure()
+    ManyBody()
+    return
+
+def Nanostructure():
+    #zwraca energię i funkcje falowe 3D nanostruktury
+    return
+
+def Coulomb(Psi):
+    #zwraca Elementy macierzowe <m|V_coul|n>, gdzie n,m odpowiada funkcjom falowym Psi
+    return
+
+def ManyBody():
+    #Znajduje energie układu wielocząstkowego
+    return
+
+if __name__ == "__main__":
+    main()
+    
